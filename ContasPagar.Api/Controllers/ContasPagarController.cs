458f8d6173a9f0130811bfd3e73f8b64fff086f4
José Campelo <josecampelojr@gmail.com>
2025-10-01 17:24:23 -0300

    feat: Define a camada de apresentação

---
+++ ContasPagar.Api/Controllers/ContasPagarController.cs
@@ -0,0 +1,78 @@
+﻿using ContasPagar.Api.DTOs;
+using ContasPagar.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ContasPagar.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/contas")]
+    public class ContasPagarController : ControllerBase
+    {
+        private readonly IContaPagarService _contaPagarService;
+
+        public ContasPagarController(IContaPagarService contaPagarService)
+        {
+            _contaPagarService = contaPagarService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ContaPagarDto>> ListarTodas()
+        {
+            var contas = _contaPagarService.ListarContasPagar();
+            return Ok(contas);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ContaPagarDto> ObterPorId(int id)
+        {
+            var conta = _contaPagarService.ObterContaPagarPorId(id);
+
+            if (conta == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(conta);
+        }
+
+        [HttpPost]
+        public ActionResult<ContaPagarDto> Criar([FromBody] CriarContaPagarDto criarDto)
+        {
+            var novaConta = _contaPagarService.CriarContaPagar(criarDto);
+
+            return CreatedAtAction(nameof(ObterPorId), new { id = novaConta.Id }, novaConta);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, [FromBody] AtualizarContaPagarDto atualizarDto)
+        {
+            if (id != atualizarDto.Id)
+            {
+                return BadRequest();
+            }
+
+            var contaAtualizada = _contaPagarService.AtualizarContaPagar(atualizarDto);
+
+            if (contaAtualizada == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Excluir(int id)
+        {
+            var contaExcluida = _contaPagarService.ExcluirContaPagar(id);
+
+            if (contaExcluida == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
