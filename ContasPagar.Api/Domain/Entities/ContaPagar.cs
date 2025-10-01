26e9911804666bd1d23e8454598b93357fa0a977
José Campelo <josecampelojr@gmail.com>
2025-10-01 15:38:00 -0300

    feat: Define a entidade ContaPagar

---
+++ ContasPagar.Api/Domain/Entities/ContaPagar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ContasPagar.Api.Domain.Entities
+{
+    public class ContaPagar
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal ValorOriginal { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public decimal ValorCorrigido { get; set; }
+        public int QuantidadeDiasAtraso { get; set; }
+        public decimal PercentualMultaAplicado { get; set; }
+        public decimal PercentualJurosDiaAplicado { get; set; }
+    }
+}
